907bc6d2d4090252ff7234528db204908d11b274
Paul Spence <hello@paulspence.me.uk>
2025-10-24 14:11:34 +0100

    TDS: extracting the api key into an env

--- src/components/ui/CurrencyConverter/CurrencyConverter.queries.ts
+++ src/components/ui/CurrencyConverter/CurrencyConverter.queries.ts
@@ -1,12 +1,14 @@
 import type { Currency, CurrencyResponse } from "./CurrencyConverter.types";
 
+const API_KEY = import.meta.env.CURRENCY_API_KEY || "WrpNSkWSHysvhrtAParCj0f49dOkFT61"; // Old api key for testing and fallback
+
 export const convertAmount = async (
     amount: number,
     from: string,
     to: string
 ): Promise<number> => {
     const response = await fetch(
-        `https://api.currencybeacon.com/v1/convert?api_key=WrpNSkWSHysvhrtAParCj0f49dOkFT61&amount=${amount}&from=${from}&to=${to}`
+        `https://api.currencybeacon.com/v1/convert?api_key=${API_KEY}&amount=${amount}&from=${from}&to=${to}`
     );
     const data = await response.json();
     return data.response.value;
@@ -14,7 +16,7 @@ export const convertAmount = async (
 
 export const getCurrencies = async (): Promise<Currency[]> => {
     const response = await fetch(
-        "https://api.currencybeacon.com/v1/currencies?api_key=WrpNSkWSHysvhrtAParCj0f49dOkFT61&type=fiat"
+        `https://api.currencybeacon.com/v1/currencies?api_key=${API_KEY}&type=fiat`
     );
     const data = (await response.json()) as CurrencyResponse;
     return data.response;
